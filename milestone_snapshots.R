3ccfdc7da0a7b6efb4ed4a8d9203efa4c3aed6aa
Ieuan Israel <ieuan.israel@cricketnsw.com.au>
2026-09-25 18:13:18 +1000

    changes for ludis

--- milestone_snapshots.R
+++ milestone_snapshots.R
@@ -1,26 +1,19 @@
 library(glue)
 library(lubridate)
 library(dplyr)
+library(odbc)
 
 Sys.setenv("MILESTONES_USE_SAMPLE" = 0) # uncomment for database
 Sys.setenv("MILESTONES_USE_LUDIS" = 0)
-
-if (!file.exists("app.R")) {
-  stop("Run this script from the milestones_dev project root.")
-}
 
 team <- "'NSW Blues M'"
 series <- 4
 series_name <- "Aus Domestic OD M"
 season <- "2025-26"
 
-source("./R/config/milestone_def.R")
-source("./R/config/select_choices.R")
-if (file.exists("./R/config/constants.R")) {
-  source("./R/config/constants.R")
-} else {
-  source("./R/config/local_constants.R")
-}
+source("./milestone_def.R")
+source("./select_choices.R")
+source("./constants.R")
 
 filters <- list(
   series = series,
@@ -28,14 +21,14 @@ filters <- list(
   team = NULL
 )
 
-source("./R/database/connection.R")
-source("./R/database/auth.R")
-source("./R/database/filters.R")
-source("./R/database/lookups.R")
-source("./R/database/local_data.R")
-source("./R/milestone_functions/milestone_helpers.R")
-source("./email/email_milestone_helpers.R")
-source("./email/email_query_builder.R")
+source("./connection.R")
+source("./auth.R")
+source("./filters.R")
+source("./lookups.R")
+source("./local_data.R")
+source("./milestone_helpers.R")
+source("./email_milestone_helpers.R")
+source("./email_query_builder.R")
 
 enabled <- milestones_for_series(filters$series)
 
@@ -62,7 +55,7 @@ results <- purrr::map_df(seq_len(nrow(enabled)), function(i) {
 
 })
 
-source("./R/milestone_functions/query_builders.R")
+source("./query_builders.R")
 
 progress <- purrr::map_df(seq_len(nrow(enabled)), function(i) {
   definition <- enabled[i, ]
@@ -105,7 +98,7 @@ if (Sys.getenv("MILESTONES_USE_LUDIS") == 1) {
 }
 
 
-source("./email/email_html.R")
+source("./email_html.R")
 message("Wrote player_milestone_update.html")
 
 
---
+++ requirements-r.R
@@ -0,0 +1,5 @@
+tidyverse
+glue
+DBI
+odbc
+AzureAuth
