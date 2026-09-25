e43c77c395f1250ba5bfc98dc5e7a94fd9e8e5e7
Ieuan Israel <ieuan.israel@cricketnsw.com.au>
2026-09-25 18:54:50 +1000

    install script for ludis

---
+++ install.sh
@@ -0,0 +1,8 @@
+R -e "install.packages('tidyverse')";
+R -e "install.packages('glue')";
+R -e "install.packages('DBI')";
+R -e "install.packages('odbc')";
+R -e "install.packages('AzureAuth')";
+R -e "install.packages('htmltools')";
+R -e "install.packages('stringr')";
+R -e "tinytex::install_tinytex()";
